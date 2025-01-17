a609f6206b70478e204897e522373342d47eb040
emiliabollenbeck <em871bol@htwg-konstanz.de>
2025-01-17 14:56:28 +0100

    "test bug fixed"

--- src/test/scala/ControllerTest/CommandTest/KnockCommandTest.scala
+++ src/test/scala/ControllerTest/CommandTest/KnockCommandTest.scala
@@ -1,4 +1,5 @@
 package ControllerTest.CommandTest
+
 import Controller.GameBuilder.GameBuilder
 import Controller.Controller
 import FileIO.FileIOImpl.FileIOJSON
@@ -7,41 +8,57 @@ import Model.BaseImpl.{Card, CardDeck, GameState, User}
 import org.scalatest.wordspec.AnyWordSpec
 
 class KnockCommandTest extends AnyWordSpec {
-  
-  "A KnockCommand" should {
-    
-    "increment knockCounter and set gameOver" in {
-      val player1 = User(Seq(Card("Herz", "7"), Card("Pik", "10"), Card("Karo", "K")), 1, "Player1")
-      val player2 = User(Seq(Card("Kreuz", "7"), Card("Herz", "10"), Card("Herz", "K")), 3, "Player2")
-      val table = User(Seq.empty, -1, "Table")
-      val gameState = GameState(
-        players = Seq(player1, player2),
-        table = table,
-        deck = new CardDeck().shuffleDeck(),
-        queue = 1,
-        knockCounter = 1,
-        gameOver = false
-      )
 
-      val controller = Controller(new GameBuilder {
-        override def returnGameState(): GameState = gameState
-        override def updateTable(): User = table
-        override def createCardDeck(): CardDeck = new CardDeck().shuffleDeck()
-      }, new FileIOJSON)
-      
+  val player1 = User(Seq(Card("Herz", "7"), Card("Pik", "10"), Card("Karo", "K")), 1, "Player1")
+  val player2 = User(Seq(Card("Kreuz", "7"), Card("Herz", "10"), Card("Herz", "K")), 3, "Player2")
+  val table = User(Seq.empty, -1, "Table")
+  val gameState = GameState(
+    players = Seq(player1, player2),
+    table = table,
+    deck = new CardDeck().shuffleDeck(),
+    queue = 1,
+    knockCounter = 1,
+    gameOver = false
+  )
+
+  val controller = Controller(new GameBuilder {
+    override def returnGameState(): GameState = gameState
+    override def updateTable(): User = table
+    override def createCardDeck(): CardDeck = new CardDeck().shuffleDeck()
+  }, new FileIOJSON)
+
+  "A KnockCommand" should {
+
+    "increment knockCounter and set gameOver" in {
       val knock = KnockCommand(controller)
       knock.execute()
 
       assert(controller.gameState.knockCounter == 2, "Der knockCounter sollte um 1 erhöht werden.")
       assert(controller.gameState.gameOver, "Das Spiel sollte vorbei sein.")
     }
-    
+
     "undo the step" in {
-      // TODO
+      val knock = KnockCommand(controller)
+      knock.execute()
+
+      knock.undoStep()
+      val updatedKnockCounter = controller.gameState.knockCounter
+      val GameOver = controller.gameState.gameOver
+
+      assert(controller.gameState.knockCounter == updatedKnockCounter, "Der knockCounter sollte zurückgesetzt worden sein.")
+      assert(controller.gameState.gameOver == GameOver, "Das Spiel sollte nicht mehr vorbei sein.")
     }
-    
+
     "redo the step" in {
-      // TODO
+      val knock = KnockCommand(controller)
+      knock.execute()
+
+      knock.undoStep()
+      val updatedKnockCounter = controller.gameState.knockCounter
+      knock.redoStep()
+
+      assert(controller.gameState.knockCounter != updatedKnockCounter, "Der knockCounter sollte wieder auf 2 erhöht worden sein.")
+      assert(controller.gameState.gameOver, "Das Spiel sollte wieder vorbei sein.")
     }
   }
 }
--- src/test/scala/ControllerTest/CommandTest/TradeOneCommandTest.scala
+++ src/test/scala/ControllerTest/CommandTest/TradeOneCommandTest.scala
@@ -9,25 +9,27 @@ import FileIO.FileIOImpl.FileIOJSON
 
 class TradeOneCommandTest extends AnyWordSpec {
 
+  val player = User(Seq(Card("Herz", "7"), Card("Pik", "10"), Card("Karo", "K")), 1, "Player")
+  val table = User(Seq(Card("Karo", "8"), Card("Herz", "9"), Card("Kreuz", "A")), -1, "Table")
+  val gameState = GameState(
+    players = Seq(player),
+    table = table,
+    deck = new CardDeck().shuffleDeck(),
+    indexCardPlayer = 0,
+    indexCardTable = 0
+  )
+
+  val controller = Controller(new GameBuilder {
+    override def returnGameState(): GameState = gameState
+
+    override def updateTable(): User = table
+
+    override def createCardDeck(): CardDeck = new CardDeck().shuffleDeck()
+  }, new FileIOJSON)
+
   "A TradeOneCommand" should {
 
     "change one player card with one table card" in {
-      val player = User(Seq(Card("Herz", "7"), Card("Pik", "10"), Card("Karo", "K")), 1, "Player")
-      val table = User(Seq(Card("Karo", "8"), Card("Herz", "9"), Card("Kreuz", "A")), -1, "Table")
-      val gameState = GameState(
-        players = Seq(player),
-        table = table,
-        deck = new CardDeck().shuffleDeck(),
-        indexCardPlayer = 0,
-        indexCardTable = 0
-      )
-
-      val controller = Controller(new GameBuilder {
-        override def returnGameState(): GameState = gameState
-        override def updateTable(): User = table
-        override def createCardDeck(): CardDeck = new CardDeck().shuffleDeck()
-      }, new FileIOJSON)
-
       val tradeOneCommand = TradeOneCommand(controller)
       tradeOneCommand.execute()
 
@@ -39,11 +41,28 @@ class TradeOneCommandTest extends AnyWordSpec {
     }
 
     "undo the step" in {
-      // TODO
+      val tradeOne = TradeOneCommand(controller)
+      tradeOne.execute()
+
+      tradeOne.undoStep()
+      val updatedPlayer = controller.gameState.players.find(_.name == "Player").get
+      val updatedTable = controller.gameState.table
+
+      assert(updatedPlayer.handDeck != Seq(Card("Herz", "7"), Card("Pik", "10"), Card("Karo", "K")), "Der Spieler sollte wieder seine alte Karte haben")
+      assert(updatedTable.handDeck != Seq(Card("Karo", "8"), Card("Herz", "9"), Card("Kreuz", "A")), "Der Tisch sollte wieder seine alte Karte haben")
     }
 
     "redo the step" in {
-      // TODO
+      val tradeOne = TradeOneCommand(controller)
+      tradeOne.execute()
+
+      tradeOne.undoStep()
+      val updatedPlayer = controller.gameState.players.find(_.name == "Player").get
+      val updatedTable = controller.gameState.table
+      tradeOne.redoStep()
+
+      assert(updatedPlayer.handDeck == Seq(Card("Pik", "10"), Card("Karo", "K"), Card("Karo", "8")), "Der Spieler sollte wieder eine Karte vom Tisch haben")
+      assert(updatedTable.handDeck == Seq(Card("Herz", "9"), Card("Kreuz", "A"), Card("Herz", "7")), "Der Tisch sollte wieder eine Karte vom Spieler haben")
     }
   }
 }
